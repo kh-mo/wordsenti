5d7db9ba9098e2e841fa580e67cb5eafd6f3945d
mosi <momkdh2@naver.com>
2019-07-27 19:44:02 +0900

    tokenized_list 추가

--- preprocessing.py
+++ preprocessing.py
@@ -1,17 +1,20 @@
 import os
 import json
+import pickle
 from konlpy.tag import Okt
 
 if __name__ == "__main__":
     okt = Okt()
 
+    tokenized_list = []
     with open(os.path.join(os.getcwd(), 'raw_data/data.json'), encoding='utf-8') as r:
         with open(os.path.join(os.getcwd(), 'raw_data/preprocessed_data.json'), "w", encoding='utf-8') as w:
             for idx, line in enumerate(r):
                 file = json.loads(line)
                 review = file['review'].strip()
-                # get tokenized file &
+                # get tokenized file
                 tokenize_review = okt.morphs(review)
+                tokenized_list.append(tokenize_review)
 
                 # write to json file
                 w.write(json.dumps({"score": file['score'],
@@ -19,6 +22,11 @@ if __name__ == "__main__":
                                     "tokenized_review": tokenize_review}, ensure_ascii=False))
                 w.write("\n")
 
+
                 # show processing
                 if idx % 1000 == 0:
                     print("preprocessing :", idx)
+
+    # save tokenized data for modeling word2vec
+    with open(os.path.join(os.getcwd(), 'raw_data/tokenized'), "wb") as f:
+        pickle.dump(tokenized_list, f)
